bad73d30c36dffdca67b10bc214ceaa89878fe41
chengchaos <chengchaos@gmail.com>
2015-11-23 15:38:57 +0800

    1

---
+++ simple-utils/src/main/java/so/chaos/simple/utils/App.java
@@ -0,0 +1,13 @@
+package so.chaos.simple.utils;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
